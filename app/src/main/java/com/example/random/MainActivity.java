5f8152b6b9fb8fcc7da2bd06a5d7e93f8a3f4315
cadamowicz <claudio@cwbtech.com.br>
2023-03-25 17:29:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/random/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.random;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvaleatorio;
+    Button aleatorios;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvaleatorio=findViewById(R.id.tvaleatorio);
+        aleatorios=findViewById(R.id.aleatorios);
+
+        aleatorios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numerosAleatorios();
+            }
+        });
+    }
+
+    private void numerosAleatorios() {
+        String aux="";
+
+        int[] numeros = new int[10];
+
+        for (int i=0; i<numeros.length;i++){
+            numeros[i]= (int) (Math.random() *10);
+
+            for( int j=i+1;j< numeros.length;j++){
+
+                if(numeros[i]==numeros[j]){
+                    aux= "igual" ;
+                }
+
+            }
+            aux=aux + (i+1) + "=" + numeros[i] + "\n";
+
+        }
+        tvaleatorio.setText(aux);
+    }
+}
